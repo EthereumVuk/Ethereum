19d0e6e04682f6edf8268583814608d16c82e4ca
Vuk Djukic <vukdukic@gmail.com>
2018-05-01 11:50:45 -0700

    Update FirstContract.sol

--- FirstContract.sol
+++ FirstContract.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.4.17;
 
+/* Defines a new contract that will have methods and variables */
 contract Inbox {
+    /* Declare all of the instance variables (and their types) that will exist in this contract */
     string public message;
     
+    
+    /* Defines different functions that will be members of the conract */
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
